bd14156b428810b135c3c1e5368458c27bbd2168
Ray Wang <hsiawang@amazon.com>
2021-04-24 13:25:36 +0800

    Init project

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'd0074bd16f37ff61103b5def391ce271744ac00b',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd0074bd16f37ff61103b5def391ce271744ac00b',
+    ),
+  ),
+);
